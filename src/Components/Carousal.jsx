f15b72ae6019a7c27bc4a27aeceb42c1fd17f540
Jilshak <mohd.jilshak@gmail.com>
2023-09-23 20:53:57 +0530

    carousal is working now

--- src/Components/Carousal.jsx
+++ src/Components/Carousal.jsx
@@ -1,52 +1,49 @@
-    import React from 'react'
+import React, { useState } from "react";
 
-    function Carousal() {
-        return (
 
-            <div id="default-carousel" class="relative w-full" data-carousel="slide">
-                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://media.istockphoto.com/id/1335286934/photo/baggage-claim-in-airport-terminal-suitcases-on-the-airport-luggage-conveyor-belt.webp?b=1&s=170667a&w=0&k=20&c=Q11Gb69c8vssk11qYswD0x5q6ygdFTNCb1NDWT20UMk=" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                    </div>
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://media.istockphoto.com/id/1335286934/photo/baggage-claim-in-airport-terminal-suitcases-on-the-airport-luggage-conveyor-belt.webp?b=1&s=170667a&w=0&k=20&c=Q11Gb69c8vssk11qYswD0x5q6ygdFTNCb1NDWT20UMk=" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                    </div>
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://media.istockphoto.com/id/1335286934/photo/baggage-claim-in-airport-terminal-suitcases-on-the-airport-luggage-conveyor-belt.webp?b=1&s=170667a&w=0&k=20&c=Q11Gb69c8vssk11qYswD0x5q6ygdFTNCb1NDWT20UMk=" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                    </div>
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://media.istockphoto.com/id/1335286934/photo/baggage-claim-in-airport-terminal-suitcases-on-the-airport-luggage-conveyor-belt.webp?b=1&s=170667a&w=0&k=20&c=Q11Gb69c8vssk11qYswD0x5q6ygdFTNCb1NDWT20UMk=" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                    </div>
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://media.istockphoto.com/id/1335286934/photo/baggage-claim-in-airport-terminal-suitcases-on-the-airport-luggage-conveyor-belt.webp?b=1&s=170667a&w=0&k=20&c=Q11Gb69c8vssk11qYswD0x5q6ygdFTNCb1NDWT20UMk=" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                    </div>
-                </div>
-                <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                </div>
-                <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </button>
-            </div>
 
-        )
+function Carousal() {
+
+    const carousal = [
+      {
+        url: 'https://media.istockphoto.com/id/534577083/photo/family-on-carousel-in-amusement-park.webp?b=1&s=612x612&w=0&k=20&c=wILwuWcPpPFIvoKboRYTP-u8wYZ8T7a5-MIqLHEu08A='
+      },
+      {
+        url:'https://media.istockphoto.com/id/485036970/photo/cute-kids-having-fun-riding-on-a-colorful-carnival-carousel.webp?b=1&s=170667a&w=0&k=20&c=UhPLkbtaCw43dypZJsdbb0Akpd-BxCGHJ_1GJ_GUU40='
+      },
+      {
+        url: 'https://media.istockphoto.com/id/534577083/photo/family-on-carousel-in-amusement-park.webp?b=1&s=612x612&w=0&k=20&c=wILwuWcPpPFIvoKboRYTP-u8wYZ8T7a5-MIqLHEu08A='
+      },
+      {
+        url: 'https://media.istockphoto.com/id/534577083/photo/family-on-carousel-in-amusement-park.webp?b=1&s=612x612&w=0&k=20&c=wILwuWcPpPFIvoKboRYTP-u8wYZ8T7a5-MIqLHEu08A='
+      }
+    ]
+  
+    const [currentIndex, setCurrentIndex] = useState(3);
+  
+    const prevSlide = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? carousal.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
     }
-
-    export default Carousal
+  
+    const nextSlide = () => {
+      const isLastSlide = currentIndex == carousal.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    }
+  
+    return (
+      <div className='w-full flex justify-center items-center h-[40vh]'>
+        <div style={{ backgroundImage: `url(${carousal[currentIndex].url})` }} className='w-11/12 cursor-pointer h-[30vh] bg-cover group rounded-md'>
+          {/* left arrow */}
+          <button onClick={prevSlide} className='hidden group-hover:block relative top-[95px]  left-[-30px] cursor-pointer'>left</button>
+          {/* right arrow */}
+          <button onClick={nextSlide} className='hidden group-hover:block relative top-[40px] h-auto left-[30px] float-right cursor-pointer'>right</button>
+  
+        </div>
+      </div>
+    )
+  }
+  
+  export default Carousal
