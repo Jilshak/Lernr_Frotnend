bf8659786b579e9fc52d1b93d7580899b3421d4a
Jilshak <mohd.jilshak@gmail.com>
2023-10-02 23:15:17 +0530

    fixed the rating component

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -20,18 +20,18 @@ function Rating(props) {
                             ))
                         }
                     </div> :
-                    <div className="rating rating-xs relative right-2 rating-half">
+                    <div className="rating rating-xs relative right-2 bottom-1 rating-half">
                         <input type="radio" name="rating-10" className="rating-hidden" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-1" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-2" />
-                        <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-1" checked />
-                        <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-2" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-1" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-2" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-1" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-2" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-1" />
                         <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-2" />
+                        <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-1" />
+                        <input type="radio" name="rating-10" className="bg-[#FEE9D8] mask mask-star-2 mask-half-2" checked/>
                     </div>
             }
         </>
