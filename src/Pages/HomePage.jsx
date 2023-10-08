ee89c373dddfbade4d655efdf0bde434e6a3b716
Jilshak <mohd.jilshak@gmail.com>
2023-10-08 10:20:16 +0530

    user blocking completed

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,15 +5,13 @@ import CategoriesCards from '../Components/CategoriesCards'
 import Footer from '../Components/Footer'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCategories, getCourses } from '../features/CourseSlice'
-import jwtDecode from 'jwt-decode'
+
+
 
 function HomePage() {
 
     const dispatch = useDispatch()
     const course = useSelector((state) => state.courses)
-
-    const access = jwtDecode(localStorage.getItem('authToken'))
-
 
     useEffect(() => {
         dispatch(getCourses(access.user_id))
@@ -21,51 +19,53 @@ function HomePage() {
     }, [])
 
     return (
-        <div className='h-full'>
-            <div className='mx-[30px] mt-3 z-0'>
-                <Carousal />
-            </div>
-            {
-                course && course.data.length >= 1 ?
-                    <>
-                        <div className='mt-4 h-[400px] mx-[30px]'>
-                            <h1 className='text-2xl font-bold text-[#3D3D3D] '>Popular Courses </h1>
-                            <div className='h-[300px] flex carousel carousel-center rounded-box bg-white relative top-5  overflow-x-auto overflow-y-hidden' style={{ maxWidth: '100%' }}>
-                                {
-                                    [...course.data].reverse()?.map((item) => {
-                                        return (
-                                            <CoursesCards key={item.id} item={item} top={3} />
-                                        )
-                                    })
-                                }
+        <>
+            <div className='h-full'>
+                <div className='mx-[30px] mt-3 z-0'>
+                    <Carousal />
+                </div>
+                {
+                    course && course.data.length >= 1 ?
+                        <>
+                            <div className='mt-4 h-[400px] mx-[30px]'>
+                                <h1 className='text-2xl font-bold text-[#3D3D3D] '>Popular Courses </h1>
+                                <div className='h-[300px] flex carousel carousel-center rounded-box bg-white relative top-5  overflow-x-auto overflow-y-hidden' style={{ maxWidth: '100%' }}>
+                                    {
+                                        [...course.data].reverse()?.map((item) => {
+                                            return (
+                                                <CoursesCards key={item.id} item={item} top={3} />
+                                            )
+                                        })
+                                    }
 
+                                </div>
                             </div>
-                        </div>
-                    </> : null
-            }
+                        </> : null
+                }
 
-            {
-                course && course.category.length >= 1 ?
-                    <>
-                        <div className='mx-[30px]'>
-                            <h1 className='text-2xl font-bold text-[#3D3D3D] '>Top Categories</h1>
-                            <div className='grid lg:grid-cols-4 md:grid-cols-3 justify-center sm:grid-cols-2 gap-20 my-8'>
-                                {
-                                    course.category.map((item) => {
-                                        return (
-                                            <CategoriesCards key={item.id} item={item} />
-                                        )
-                                    })
-                                }
+                {
+                    course && course.category.length >= 1 ?
+                        <>
+                            <div className='mx-[30px]'>
+                                <h1 className='text-2xl font-bold text-[#3D3D3D] '>Top Categories</h1>
+                                <div className='grid lg:grid-cols-4 md:grid-cols-3 justify-center sm:grid-cols-2 gap-20 my-8'>
+                                    {
+                                        course.category.map((item) => {
+                                            return (
+                                                <CategoriesCards key={item.id} item={item} />
+                                            )
+                                        })
+                                    }
 
+                                </div>
                             </div>
-                        </div>
-                    </> : null
-            }
-            <div>
-                <Footer />
+                        </> : null
+                }
+                <div>
+                    <Footer />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/Pages/RootPage.jsx
+++ src/Pages/RootPage.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../Components/Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import jwtDecode from 'jwt-decode'
+import { getMyProfile } from '../features/UserSlice'
+
+
 
 function RootPage() {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const access = jwtDecode(localStorage.getItem('authToken'))
+
+    const blocking = useSelector((state) => state.users)
+
+    const handleLogout = () => {
+        localStorage.removeItem('authToken')
+        navigate('/login')
+    }
+
+    useEffect(() => {
+        dispatch(getMyProfile(access.user_id))
+    },[])
+
+
     return (
         <>
-            <div className='sticky top-0 z-50'>
-                <Navbar />
-            </div>
-            <div>
-                <Outlet />
-            </div>
+            {
+                blocking && !blocking.profile.is_blocked || localStorage.getItem('guestToken') ?
+                    <>
+                        <div className='sticky top-0 z-50'>
+                            <Navbar />
+                        </div>
+                        <div>
+                            <Outlet />
+                        </div>
+                    </> :
+                    <div className='flex h-screen items-center justify-center'>
+                        <div>
+                            <h1 className='text-2xl'>You have been blocked by the admin!!</h1>
+                            <button onClick={handleLogout} className="btn btn-outline w-full mt-5">LogOut</button>
+                        </div>
+                    </div>
+            }
+
         </>
     )
 }
